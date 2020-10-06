8fca387b38d26da67f4289032cad4129c688badb
Cameron Bisca <cjbisca@gmail.com>
2020-10-06 19:23:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == false 
+  if position_taken?(board, index) == false && 
     return true 
   elsif index > 8 && index < 0  
     return false 
