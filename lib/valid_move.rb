d15329a8e1da56172891c5a448dd693f545e70ea
Cameron Bisca <cjbisca@gmail.com>
2020-10-06 18:52:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
   if position_taken?(board, index) == false 
     return true 
   
-  else 
+  else position_taken?(board, index) == true
     return false  
   end
 end
